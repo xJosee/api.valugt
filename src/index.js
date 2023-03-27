d523261fef94d6eac4998a61063aa9b107ccba28
xJosee <3006762980101@ingenieria.usac.edu.gt>
2023-03-27 15:42:55 -0600

    path

--- src/index.js
+++ src/index.js
@@ -21,8 +21,8 @@ app.get('/', (req, res) => {
 });
 
 const options = {
-  key: fs.readFileSync(path.resolve('selfsigned.key')),
-  cert: fs.readFileSync(path.resolve('selfsigned.crt'))
+  key: fs.readFileSync(path.resolve('/src/selfsigned.key')),
+  cert: fs.readFileSync(path.resolve('/src/selfsigned.crt'))
 };
 
 https.createServer(options, app).listen(PORT);
