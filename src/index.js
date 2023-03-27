0fb0841656ed3692743222e8b46d5f580aa9645f
xJosee <3006762980101@ingenieria.usac.edu.gt>
2023-03-27 15:28:17 -0600

    https

--- src/index.js
+++ src/index.js
@@ -4,18 +4,24 @@ const dpiRoutes = require('./routes/dpi.route');
 const licenciaRoutes = require('./routes/licencia.route');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const https = require('https');
+const fs = require('fs');
+
+const PORT = 43;
 
 //middlewares
 app.use(cors());
 app.use(express.json());
-app.use('/dpi', dpiRoutes)
-app.use('/licencia', licenciaRoutes)
+app.use('/dpi', dpiRoutes, cors())
+app.use('/licencia', licenciaRoutes, cors())
 
 app.get('/', (req, res) => {
     res.send('valugt API');
 });
 
-app.listen(PORT, () => {
-    console.log('Server started on port',  PORT);
-});
+const options = {
+  key: fs.readFileSync('../../ssl/selfsigned.key'),
+  cert: fs.readFileSync('../../ssl/selfsigned.crt')
+};
+
+https.createServer(options, app).listen(PORT);
